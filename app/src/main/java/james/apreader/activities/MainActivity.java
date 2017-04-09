4d9fddac38573ec6ee2d541c4a8c032f0a6f0518
James Fenn <18jafenn90@gmail.com>
2017-04-09 19:19:14 -0400

    switched to bottom navigation

--- app/src/main/java/james/apreader/activities/MainActivity.java
+++ app/src/main/java/james/apreader/activities/MainActivity.java
@@ -1,100 +1,41 @@
 package james.apreader.activities;
 
 import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.content.ContextCompat;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.mikepenz.materialdrawer.AccountHeaderBuilder;
-import com.mikepenz.materialdrawer.Drawer;
-import com.mikepenz.materialdrawer.DrawerBuilder;
-import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
-import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
-
-import james.apreader.BuildConfig;
 import james.apreader.R;
-import james.apreader.common.Supplier;
 import james.apreader.fragments.FavFragment;
 import james.apreader.fragments.ListFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-    Toolbar toolbar;
-    View fragmentView;
-    Fragment fragment;
-    Drawer drawer;
-
-    Supplier supplier;
+    private Toolbar toolbar;
+    private View fragmentView;
+    private Fragment fragment;
+    private BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        supplier = (Supplier) getApplicationContext();
-
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         fragmentView = findViewById(R.id.fragment);
+        navigationView = (BottomNavigationView) findViewById(R.id.navigation);
 
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.drawer_toggle);
 
-        drawer = new DrawerBuilder()
-                .withActivity(this)
-                .withFullscreen(true)
-                .withTranslucentStatusBar(true)
-                .withToolbar(toolbar)
-                .withAccountHeader(new AccountHeaderBuilder()
-                        .withActivity(this)
-                        .withTranslucentStatusBar(true)
-                        .withCompactStyle(false)
-                        .withHeaderBackground(new ColorDrawable(ContextCompat.getColor(this, R.color.colorPrimary)))
-                        .withProfileImagesClickable(false)
-                        .withSelectionListEnabledForSingleProfile(false)
-                        .withProfileImagesVisible(false)
-                        .addProfiles(new ProfileDrawerItem().withName(getResources().getString(R.string.app_name)).withEmail("Version " + BuildConfig.VERSION_NAME))
-                        .build())
-                .addDrawerItems(
-                        new SecondaryDrawerItem().withName(getString(R.string.title_articles)).withIdentifier(0).withIcon(R.drawable.ic_inbox),
-                        new SecondaryDrawerItem().withName(getString(R.string.title_favorites)).withIdentifier(1).withIcon(R.drawable.ic_fav)
-                )
-                .addDrawerItems(
-                        new SecondaryDrawerItem().withName(getString(R.string.title_about)).withIdentifier(2).withSelectable(false).withIcon(R.drawable.ic_info)
-                )
-                .withSelectedItem(0)
-                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
-                    @Override
-                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-                        int id = (int) drawerItem.getIdentifier();
-
-                        switch ((int) drawerItem.getIdentifier()) {
-                            case 0:
-                                fragment = new ListFragment();
-                                toolbar.setTitle(getString(R.string.title_articles));
-                                break;
-                            case 1:
-                                fragment = new FavFragment();
-                                toolbar.setTitle(getString(R.string.title_favorites));
-                                break;
-                            case 2:
-                                startActivity(new Intent(MainActivity.this, AboutActivity.class));
-                                break;
-                        }
-
-                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragment).commit();
-                        drawer.closeDrawer();
-                        return false;
-                    }
-                }).build();
+        navigationView.setOnNavigationItemSelectedListener(this);
 
         getSupportFragmentManager().addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
             @Override
@@ -118,11 +59,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setSelection(Fragment fragment) {
-        if (fragment instanceof ListFragment) {
-            drawer.setSelection(-1);
-        } else if (fragment instanceof FavFragment) {
-            drawer.setSelection(-2);
-        }
+        if (fragment instanceof ListFragment)
+            navigationView.setSelectedItemId(R.id.action_articles);
+        else if (fragment instanceof FavFragment)
+            navigationView.setSelectedItemId(R.id.action_favorites);
     }
 
     @Override
@@ -134,13 +74,41 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case android.R.id.home:
-                drawer.openDrawer();
-                break;
             case R.id.action_search:
                 startActivity(new Intent(MainActivity.this, SearchActivity.class));
                 break;
+            case R.id.action_about:
+                startActivity(new Intent(MainActivity.this, AboutActivity.class));
+                break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_articles:
+                fragment = new ListFragment();
+                toolbar.setTitle(getString(R.string.title_articles));
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment, fragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                        .commit();
+
+                return true;
+            case R.id.action_favorites:
+                fragment = new FavFragment();
+                toolbar.setTitle(getString(R.string.title_favorites));
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment, fragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                        .commit();
+
+                return true;
+        }
+
+        return false;
     }
 }
